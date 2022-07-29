93614bdf7a15cc579a5256fc43cfcc3d12f7dee4
Cobey Hollier <cobey.hollier@mac.com>
2022-07-29 12:50:09 -0700

    CH: ADding basic audio manager and menu button hover sounds

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            s.source.Play();
+            return;
+        }
+        else
+        {
+            Debug.LogWarning("Sound not found with name: " + name);
+        }
+    }
+}
