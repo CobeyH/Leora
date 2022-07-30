a334122cd8e604c5af617444f2ee414a0628fcc8
Jae Park <jaepark052@gmail.com>
2022-07-30 14:59:40 -0700

    Added forcefield for moth

--- Assets/Scripts/MothLightLevelSelection.cs
+++ Assets/Scripts/MothLightLevelSelection.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class MothLightLevelSelection : MonoBehaviour
 {
     public UnityEngine.Rendering.Universal.Light2D light;
     public Button button;
+    public ParticleSystemForceField mothField;
     
     public void MouseEnter()
     {
         if (button.interactable)
         {
             light.enabled = true;
+            mothField.enabled = true;
         }
     }
 
     public void MouseLeave()
     {
         light.enabled = false;
+        mothField.enabled = false;
     }
 }
