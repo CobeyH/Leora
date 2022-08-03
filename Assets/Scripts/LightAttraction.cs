f057e944e0427f6d6d023400da95ef70a58c2c33
Cobey Hollier <cobey.hollier@mac.com>
2022-08-03 10:16:23 -0700

    CH: Cleaning up light attraction script

--- Assets/Scripts/LightAttraction.cs
+++ Assets/Scripts/LightAttraction.cs
@@ -8,50 +8,73 @@ public class LightAttraction : MonoBehaviour
     public float mothSpeed = 2f;
 
     private ParticleSystemForceField field;
+    private List<Light2D> lightsInScene;
 
     // Start is called before the first frame update
     void Start()
     {
         field = GetComponent<ParticleSystemForceField>();
+        lightsInScene = new List<Light2D>(FindObjectsOfType<Light2D>());
+
+        // Find all the global lights in the scene
+        List<Light2D> globalLights = new List<Light2D>();
+        foreach (Light2D light in lightsInScene)
+        {
+            if (light.lightType == Light2D.LightType.Global)
+            {
+                globalLights.Add(light);
+            }
+        }
+
+        // Delete the global light from the list
+        foreach (Light2D globalLight in globalLights)
+        {
+            lightsInScene.Remove(globalLight);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        var foundLights = FindObjectsOfType<Light2D>();
-        if (foundLights.Length <= 0)
+        if (lightsInScene.Count <= 0)
         {
             return;
         }
 
+        // Calculate the net force applied to the moths by all lights
         Vector2 netForce = Vector2.zero;
         int layer_mask = LayerMask.GetMask("Default");
-        foreach (Light2D light in foundLights)
+        foreach (Light2D light in lightsInScene)
         {
-            // Check if there is a clear line between the field and the light.
-            // Also ignore global lights
-            if (
-                light.enabled &&
-                light.lightType != Light2D.LightType.Global &&
-                !Physics2D.Linecast(transform.position, light.transform.position, layer_mask)
-            )
-            {
-                Vector2 vecToLight =
-                    light.transform.position - transform.position;
-
-                if (light.lightType == Light2D.LightType.Point && !MothsInBeam(light, vecToLight)) continue;
-                // Make moths attracted in the direction of the light.
-                // Moths are more attracted to stronger lights.
-                // Moths are less attracted to far away lights.
-                netForce +=
-                    vecToLight.normalized *
-                    (
-                    light.intensity / (Mathf.Pow(vecToLight.magnitude, 2) + 1)
-                    );
-            }
+            netForce += AddForceFromLight(light, layer_mask);
         }
         netForce = netForce.normalized * Time.deltaTime * mothSpeed;
         transform.position += new Vector3(netForce.x, netForce.y, 0);
+    }
+
+    Vector2 AddForceFromLight(Light2D light, int layer_mask)
+    {
+
+        // Check if there is a clear line between the field and the light.
+        // Also ignore global lights
+        if (
+            light.enabled &&
+            !Physics2D.Linecast(transform.position, light.transform.position, layer_mask)
+        )
+        {
+            Vector2 vecToLight =
+                light.transform.position - transform.position;
+
+            if (light.lightType == Light2D.LightType.Point && !MothsInBeam(light, vecToLight)) return Vector2.zero;
+            // Make moths attracted in the direction of the light.
+            // Moths are more attracted to stronger lights.
+            // Moths are less attracted to far away lights.
+            return vecToLight.normalized *
+               (
+               light.intensity / (Mathf.Pow(vecToLight.magnitude, 2) + 1)
+               );
+        }
+        return Vector2.zero;
     }
 
     private bool MothsInBeam(Light2D light, Vector2 vecToLight)
