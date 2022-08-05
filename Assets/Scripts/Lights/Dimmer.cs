7fd2fd4504964d387b13069874d3c31d03c485c4
Cobey Hollier <cobey.hollier@mac.com>
2022-08-05 14:36:46 -0700

    CH: Making light intensity controlled by dimmer

--- Assets/Scripts/Lights/Dimmer.cs
+++ Assets/Scripts/Lights/Dimmer.cs
@@ -5,15 +5,49 @@ public class Dimmer : MonoBehaviour
 {
     public bool canDim = false;
     public CapsuleCollider2D hitbox;
+    public Light2D controlledLight;
+    private bool dimming = false;
+    private bool inArea = true;
+    private float intensityMultiplier = 0;
+
+    void Start()
+    {
+        intensityMultiplier = controlledLight.intensity;
+    }
 
     void OnMouseDown()
     {
-        Vector3 mousePos = Input.mousePosition;
+        dimming = true;
+        inArea = true;
+    }
 
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
-        Vector3 mouseLocalCoords = hitbox.transform.InverseTransformPoint(worldPosition);
+    void OnMouseUp()
+    {
+        dimming = false;
+    }
 
-        float heightClicked = (mouseLocalCoords.y + hitbox.size.y / 2) / hitbox.size.y;
+    void OnMouseExit()
+    {
+        inArea = false;
+    }
 
+    void OnMouseEnter()
+    {
+        inArea = true;
+    }
+
+    void Update()
+    {
+        if (dimming && canDim && inArea)
+        {
+            Vector3 mousePos = Input.mousePosition;
+
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector3 mouseLocalCoords = hitbox.transform.InverseTransformPoint(worldPosition);
+
+            float heightClicked = (mouseLocalCoords.y + hitbox.size.y / 2) / hitbox.size.y;
+            controlledLight.intensity = heightClicked * intensityMultiplier;
+        }
     }
 }
+
