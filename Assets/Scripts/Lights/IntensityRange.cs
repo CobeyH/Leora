80456aaae11b3d8b06c823f8899705c2fc78af64
Cobey Hollier <cobey.hollier@mac.com>
2022-08-05 14:53:43 -0700

    CH: Implementing light attraction based on distance to light

---
+++ Assets/Scripts/Lights/IntensityRange.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class IntensityRange : MonoBehaviour
+{
+    public Light2D controlledLight;
+    void Update()
+    {
+    }
+}
