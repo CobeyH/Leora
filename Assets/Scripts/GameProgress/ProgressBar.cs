5ad3ebb6af2c627c1b9bdf44064d3812513a03f9
Cobey Hollier <cobey.hollier@mac.com>
2022-08-22 16:14:20 -0700

    CH: Initial implementation of radial progress moths

--- Assets/Scripts/GameProgress/ProgressBar.cs
+++ Assets/Scripts/GameProgress/ProgressBar.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     private List<GameObject> checkpoints = new List<GameObject>();
+    private List<Image> checkpointFills = new List<Image>();
     public GameObject checkpointPrefab;
     public ParticleSystem emitter;
     public Slider slider;
@@ -13,7 +14,8 @@ public class ProgressBar : MonoBehaviour
     public float fillSpeed;
     private float barWidth = 0;
 
-    private int previousCheckpointsCount = 0;
+    private int checkpointsFilled = 0;
+    private float currentValue = 0;
 
 
 
@@ -29,7 +31,6 @@ public class ProgressBar : MonoBehaviour
     {
         tracker = GameObject.FindGameObjectWithTag("GameManager").GetComponent<LevelProgressTracker>();
         float[] checkpointRequirements = tracker.GetCheckPointRequirements();
-        Debug.Log(checkpointRequirements);
 
         // Create checkpoint markers.
         foreach (float req in checkpointRequirements)
@@ -37,6 +38,7 @@ public class ProgressBar : MonoBehaviour
             GameObject cp = Instantiate(checkpointPrefab, new Vector3(req * barWidth - barWidth / 2f, 0, 0), Quaternion.identity);
             cp.transform.SetParent(this.transform, false);
             checkpoints.Add(cp);
+            checkpointFills.Add(cp.transform.GetChild(0).gameObject.GetComponent<Image>());
         }
 
 
@@ -45,17 +47,24 @@ public class ProgressBar : MonoBehaviour
     void Update()
     {
         float targetValue = tracker.GetLevelProgress();
-        if (slider.value < targetValue)
+        float[] checkpointReqs = tracker.GetCheckPointRequirements();
+        if (checkpointsFilled == checkpointReqs.Length)
         {
-            float previousValue = slider.value;
-            slider.value += fillSpeed * Time.deltaTime;
-            int currentCheckpointCount = tracker.GetCheckpointsCompleted();
-            if (currentCheckpointCount > previousCheckpointsCount)
+            return;
+        }
+        if (currentValue < targetValue)
+        {
+            currentValue += fillSpeed * Time.deltaTime;
+            float reqStart = checkpointsFilled == 0 ? 0 : checkpointReqs[checkpointsFilled - 1];
+            float reqEnd = checkpointsFilled == checkpointReqs.Length - 1 ? 1 : checkpointReqs[checkpointsFilled];
+            // Add fill to completed checkpoints
+            float newFill = Mathf.Min((targetValue - reqStart) / (float)reqEnd, 1);
+            checkpointFills[checkpointsFilled].fillAmount = newFill;
+            Debug.Log(reqStart + " " + reqEnd + " " + newFill);
+
+            if (newFill == 1)
             {
-                // Add fill to completed checkpoints
-                checkpoints[previousCheckpointsCount].transform.GetChild(0).gameObject.SetActive(true);
-                emitter.Emit(100);
-                previousCheckpointsCount++;
+                checkpointsFilled++;
             }
         }
     }
