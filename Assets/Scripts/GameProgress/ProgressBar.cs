8383aef2b7f195967256523b5bac3c12087b879f
Cobey Hollier <cobey.hollier@mac.com>
2022-08-23 11:42:24 -0700

    CH: Fixing math in radial progress bar

--- Assets/Scripts/GameProgress/ProgressBar.cs
+++ Assets/Scripts/GameProgress/ProgressBar.cs
@@ -6,42 +6,51 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     private List<GameObject> checkpoints = new List<GameObject>();
+
     private List<Image> checkpointFills = new List<Image>();
+
     public GameObject checkpointPrefab;
+
     public ParticleSystem emitter;
-    public Slider slider;
-    public LevelProgressTracker tracker;
-    public float fillSpeed;
-    private float barWidth = 0;
+
+    private LevelProgressTracker tracker;
+
+    public float fillSpeed = 0.3f;
+
+    private float barWidth;
 
     private int checkpointsFilled = 0;
+
     private float currentValue = 0;
-
-
 
     void Awake()
     {
-        slider = gameObject.GetComponent<Slider>();
-        // The slider should scale to the number of moths in the game
-        fillSpeed = slider.maxValue * 2;
         barWidth = gameObject.GetComponent<RectTransform>().rect.width;
     }
 
     void Start()
     {
-        tracker = GameObject.FindGameObjectWithTag("GameManager").GetComponent<LevelProgressTracker>();
+        tracker =
+            GameObject
+                .FindGameObjectWithTag("GameManager")
+                .GetComponent<LevelProgressTracker>();
         float[] checkpointRequirements = tracker.GetCheckPointRequirements();
 
         // Create checkpoint markers.
+        int i = 0;
         foreach (float req in checkpointRequirements)
         {
-            GameObject cp = Instantiate(checkpointPrefab, new Vector3(req * barWidth - barWidth / 2f, 0, 0), Quaternion.identity);
+            float spacing = barWidth / (checkpointRequirements.Length - 1);
+            GameObject cp =
+                Instantiate(checkpointPrefab,
+                new Vector3(i * spacing - barWidth / 2f, 0, 0),
+                Quaternion.identity);
             cp.transform.SetParent(this.transform, false);
-            checkpoints.Add(cp);
-            checkpointFills.Add(cp.transform.GetChild(0).gameObject.GetComponent<Image>());
+            checkpoints.Add (cp);
+            checkpointFills
+                .Add(cp.transform.GetChild(0).gameObject.GetComponent<Image>());
+            i++;
         }
-
-
     }
 
     void Update()
@@ -55,12 +64,18 @@ public class ProgressBar : MonoBehaviour
         if (currentValue < targetValue)
         {
             currentValue += fillSpeed * Time.deltaTime;
-            float reqStart = checkpointsFilled == 0 ? 0 : checkpointReqs[checkpointsFilled - 1];
-            float reqEnd = checkpointsFilled == checkpointReqs.Length - 1 ? 1 : checkpointReqs[checkpointsFilled];
+            float reqStart =
+                checkpointsFilled == 0
+                    ? 0
+                    : checkpointReqs[checkpointsFilled - 1];
+            float reqEnd =
+                checkpointsFilled == checkpointReqs.Length - 1
+                    ? 1
+                    : checkpointReqs[checkpointsFilled];
+
             // Add fill to completed checkpoints
-            float newFill = Mathf.Min((targetValue - reqStart) / (float)reqEnd, 1);
+            float newFill = Mathf.InverseLerp(reqStart, reqEnd, targetValue);
             checkpointFills[checkpointsFilled].fillAmount = newFill;
-            Debug.Log(reqStart + " " + reqEnd + " " + newFill);
 
             if (newFill == 1)
             {
@@ -68,5 +83,4 @@ public class ProgressBar : MonoBehaviour
             }
         }
     }
-
 }
