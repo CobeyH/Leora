f361cb41ec056c42437375671536c9bb95abf440
Cobey Hollier <cobey.hollier@mac.com>
2022-09-25 11:48:28 -0700

    CH: Adding colour changing script

---
+++ Assets/Scripts/Moths/MothColour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MothColour : MonoBehaviour
+{
+    public Color baseColor;
+
+    Material mat;
+
+    bool isDecoy = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = gameObject.GetComponent<ParticleSystemRenderer>().material;
+        isDecoy = gameObject.GetComponent<Goal>().IsDecoy;
+        if (isDecoy)
+        {
+            mat.color = Color.white;
+        }
+        else
+        {
+            mat.color = baseColor;
+        }
+    }
+}
