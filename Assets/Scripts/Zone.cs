2848aae29575991911b803eee5d4c6e42511e508
Cobey Hollier <cobey.hollier@mac.com>
2022-10-07 10:58:47 -0700

    CH: Adding basic light toggle based on zones

--- Assets/Scripts/Zone.cs
+++ Assets/Scripts/Zone.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Zone : MonoBehaviour
@@ -10,7 +8,7 @@ public class Zone : MonoBehaviour
     {
         if (col.gameObject.CompareTag("MothForces"))
         {
-            Debug.Log("Valid collision");
+            ZoneEvents.RaiseEvent(id);
         }
     }
 }
