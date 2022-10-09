906809fabe342d6af2d55f5d316d3b7b727901e6
Cobey Hollier <cobey.hollier@mac.com>
2022-10-09 12:01:01 -0700

    CH: Adding handle to moving block

--- Assets/Scripts/MovingBlock.cs
+++ Assets/Scripts/MovingBlock.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class MovingBlock : MonoBehaviour
 {
     public IntEventChannelSO triggerChannel;
@@ -65,5 +67,23 @@ public class MovingBlock : MonoBehaviour
     {
         Gizmos.DrawWireSphere(endPoint, 0.25f);
         Gizmos.DrawLine(transform.position, endPoint);
+    }
+}
+
+[CustomEditor(typeof (MovingBlock)), CanEditMultipleObjects]
+public class PositionHandleExampleEditor : Editor
+{
+    protected virtual void OnSceneGUI()
+    {
+        MovingBlock block = (MovingBlock) target;
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 newTargetPosition =
+            Handles.PositionHandle(block.endPoint, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(block, "Change Look At Target Position");
+            block.endPoint = newTargetPosition;
+        }
     }
 }
