9d3dc35ccf8de49b29a194647de012b91dc6d81c
Cobey Hollier <cobey.hollier@mac.com>
2022-10-10 12:34:07 -0700

    CH: Adding sprite overlay for zones

--- Assets/Scripts/Zone.cs
+++ Assets/Scripts/Zone.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Zone : MonoBehaviour
 {
@@ -7,25 +8,47 @@ public class Zone : MonoBehaviour
 
     public float triggerTime;
 
+    public GameObject UIPrefab;
+
     public IntEventChannelSO ZoneEvents;
 
     private float timeInside;
+
+    private Image ZoneUI;
+
+    void Awake()
+    {
+        GameObject ZoneUIObject =
+            Instantiate(UIPrefab, transform.position, Quaternion.identity);
+        ZoneUIObject
+            .transform
+            .SetParent(GameObject.Find("SpriteOverlay").transform);
+
+        ZoneUI = ZoneUIObject.GetComponent<Image>();
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("MothForces"))
         {
             timeInside = 0;
+            ZoneUI.fillAmount = 0;
         }
     }
 
     void OnTriggerStay2D()
     {
         timeInside += Time.deltaTime;
+        ZoneUI.fillAmount = timeInside / triggerTime;
         if (timeInside > triggerTime)
         {
             ZoneEvents.RaiseEvent (id);
             timeInside = 0;
         }
+    }
+
+    void OnTriggerExit2D()
+    {
+        ZoneUI.fillAmount = 0;
     }
 }
