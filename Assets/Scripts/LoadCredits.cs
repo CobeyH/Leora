42ea3ffd3bb809a5a0b41069f9fd2317a82ac77c
Cobey Hollier <cobey.hollier@mac.com>
2022-11-25 20:22:13 -0800

    CH: Adding script to load credits from level selection

---
+++ Assets/Scripts/LoadCredits.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCredits : MonoBehaviour
+{
+    public void LoadCreditScene()
+    {
+        LevelLoader.LoadLevel("Credits");
+    }
+}
